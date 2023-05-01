ebdcdf2cd97591c69f98555d56f9fb414cdf7068
Artem Alexeyenko <ala@sitecore.net>
2023-05-01 17:47:24 -0400

    test with no prefix on favicon

--- src/sxastarter/src/Layout.tsx
+++ src/sxastarter/src/Layout.tsx
@@ -5,11 +5,9 @@ import React from 'react';
 import Head from 'next/head';
 import { Placeholder, LayoutServiceData, Field } from '@sitecore-jss/sitecore-jss-nextjs';
 import Scripts from 'src/Scripts';
-import { applyVercelProtection, getPublicUrl } from 'lib/editing-temp/utils';
 
 // Prefix public assets with a public URL to enable compatibility with Sitecore Experience Editor.
 // If you're not supporting the Experience Editor, you can remove this.
-const publicUrl = getPublicUrl();
 
 interface LayoutProps {
   layoutData: LayoutServiceData;
@@ -31,7 +29,7 @@ const Layout = ({ layoutData }: LayoutProps): JSX.Element => {
       <Scripts />
       <Head>
         <title>{fields?.Title?.value?.toString() || 'Page'}</title>
-        <link rel="icon" href={`${applyVercelProtection(`${publicUrl}/favicon.ico`)}`} />
+        <link rel="icon" href={`favicon.ico`} />
       </Head>
 
       {/* root placeholder for the app, which we add components to using route data */}
