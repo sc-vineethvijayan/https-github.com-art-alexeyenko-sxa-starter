8a0a3008062f56b713d8a94246de8bdebaa97952
Artem Alexeyenko <ala@sitecore.net>
2023-05-01 08:36:58 -0400

    try with no assetPrefix

--- src/sxastarter/next.config.js
+++ src/sxastarter/next.config.js
@@ -27,7 +27,7 @@ const publicUrl = getPublicUrl();
  */
 const nextConfig = {
   // Set assetPrefix to our public URL
-  assetPrefix: publicUrl,
+  assetPrefix: '',
 
   // Allow specifying a distinct distDir when concurrently running app in a container
   distDir: process.env.NEXTJS_DIST_DIR || '.next',
