0c647bb2598289691d4d4e82ef5ec09ad8a8aeac
Artem Alexeyenko <ala@sitecore.net>
2023-05-31 15:37:35 -0400

    Revert "const export for componen"
    This reverts commit 57444ba3db1001b973b697404bb9ee0a09b71bd9.

--- src/sxastarter/src/byoc/BringYour.tsx
+++ src/sxastarter/src/byoc/BringYour.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
 
-const BringYour = () => {
+export default function BringYour(): JSX.Element {
   return <h2>You brought this on urself</h2>;
-};
-
-export default BringYour;
+}
