3e983db4675885d840ab83ad8123e125a7e915de
Artem Alexeyenko <ala@sitecore.net>
2023-05-31 16:29:18 -0400

    lint

--- src/sxastarter/src/byoc/SecondComponent.tsx
+++ src/sxastarter/src/byoc/SecondComponent.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
 
 export const SecondComponent = (): JSX.Element => {
-  return (<>
-  <h2>Second component!</h2>
-  <p><b>It's live!!!</b></p>
-  </>);
+  return (
+    <>
+      <h2>Second component!</h2>
+      <p>
+        <b>Its live!!!</b>
+      </p>
+    </>
+  );
 };
