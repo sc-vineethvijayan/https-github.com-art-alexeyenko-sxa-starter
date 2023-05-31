e5fb8aeae337c87c35a67c17b55d8f13cf194d55
Artem Alexeyenko <ala@sitecore.net>
2023-05-31 16:19:36 -0400

    add second component

--- src/sxastarter/src/byoc/BringYour.tsx
+++ src/sxastarter/src/byoc/BringYour.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
 
 export const BringYour = (): JSX.Element => {
-  return <h2>You brought this on urself</h2>;
+  return <h2>You brought this component on urself</h2>;
 };
---
+++ src/sxastarter/src/byoc/SecondComponent.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export const SecondComponent = (): JSX.Element => {
+  return (<>
+  <h2>Second component!</h2>
+  <p><b>It's live!!!</b></p>
+  </>);
+};
