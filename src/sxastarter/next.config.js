b63962b58aba0ec73bdc71aef0eee1b2286b0cc6
Artem Alexeyenko <ala@sitecore.net>
2023-06-07 15:58:18 -0400

    test with live mode for protection

--- src/sxastarter/next.config.js
+++ src/sxastarter/next.config.js
@@ -27,7 +27,7 @@ const publicUrl = getPublicUrl();
  */
 const nextConfig = {
   // Set assetPrefix to our public URL
-  assetPrefix: '',
+  assetPrefix: process.env.NODE_ENV === 'development' ? publicUrl : undefined,
 
   // Allow specifying a distinct distDir when concurrently running app in a container
   distDir: process.env.NEXTJS_DIST_DIR || '.next',
--- src/sxastarter/src/Layout.tsx
+++ src/sxastarter/src/Layout.tsx
@@ -5,6 +5,7 @@ import React from 'react';
 import Head from 'next/head';
 import { Placeholder, LayoutServiceData, Field } from '@sitecore-jss/sitecore-jss-nextjs';
 import Scripts from 'src/Scripts';
+import { getPublicUrl } from '@sitecore-jss/sitecore-jss-nextjs/utils';
 
 // Prefix public assets with a public URL to enable compatibility with Sitecore Experience Editor.
 // If you're not supporting the Experience Editor, you can remove this.
@@ -19,6 +20,7 @@ interface RouteFields {
 }
 
 const Layout = ({ layoutData }: LayoutProps): JSX.Element => {
+  const publicUrl = getPublicUrl();
   const { route } = layoutData.sitecore;
   const fields = route?.fields as RouteFields;
   const isPageEditing = layoutData.sitecore.context.pageEditing;
@@ -29,7 +31,7 @@ const Layout = ({ layoutData }: LayoutProps): JSX.Element => {
       <Scripts />
       <Head>
         <title>{fields?.Title?.value?.toString() || 'Page'}</title>
-        <link rel="icon" href={`favicon.ico`} />
+        <link rel="icon" href={`${publicUrl}/favicon.ico`} />
       </Head>
 
       {/* root placeholder for the app, which we add components to using route data */}
