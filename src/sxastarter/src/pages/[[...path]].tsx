27da3083bf6e7240b7a441ca0b586292ebe67644
Artem Alexeyenko <ala@sitecore.net>
2023-06-21 17:08:22 -0400

    upd catch all

--- src/sxastarter/src/pages/[[...path]].tsx
+++ src/sxastarter/src/pages/[[...path]].tsx
@@ -6,14 +6,13 @@ import {
   RenderingType,
   SitecoreContext,
   ComponentPropsContext,
-  handleEditorFastRefresh,
   EditingComponentPlaceholder,
   StaticPath,
 } from '@sitecore-jss/sitecore-jss-nextjs';
+import { handleEditorFastRefresh } from '@sitecore-jss/sitecore-jss-nextjs/utils';
 import { SitecorePageProps } from 'lib/page-props';
 import { sitecorePagePropsFactory } from 'lib/page-props-factory';
-// different componentFactory method will be used based on whether page is being edited
-import { componentFactory, editingComponentFactory } from 'temp/componentFactory';
+import { componentBuilder } from 'temp/componentBuilder';
 import { sitemapFetcher } from 'lib/sitemap-fetcher';
 import * as FEAAS from '@sitecore-feaas/clientside/react';
 import BringYour from 'src/byoc/BringYour';
@@ -29,7 +28,12 @@ FEAAS.External.registerComponent(BringYour, {
 });
 // FEAAS.External.registerComponent(SecondComponent);
 
-const SitecorePage = ({ notFound, componentProps, layoutData }: SitecorePageProps): JSX.Element => {
+const SitecorePage = ({
+  notFound,
+  componentProps,
+  layoutData,
+  headLinks,
+}: SitecorePageProps): JSX.Element => {
   useEffect(() => {
     // Since Sitecore editors do not support Fast Refresh, need to refresh editor chromes after Fast Refresh finished
     handleEditorFastRefresh();
@@ -47,7 +51,7 @@ const SitecorePage = ({ notFound, componentProps, layoutData }: SitecorePageProp
   return (
     <ComponentPropsContext value={componentProps}>
       <SitecoreContext
-        componentFactory={isEditing ? editingComponentFactory : componentFactory}
+        componentFactory={componentBuilder.getComponentFactory({ isEditing })}
         layoutData={layoutData}
       >
         {/*
@@ -57,7 +61,7 @@ const SitecorePage = ({ notFound, componentProps, layoutData }: SitecorePageProp
         {isComponentRendering ? (
           <EditingComponentPlaceholder rendering={layoutData.sitecore.route} />
         ) : (
-          <Layout layoutData={layoutData} />
+          <Layout layoutData={layoutData} headLinks={headLinks} />
         )}
       </SitecoreContext>
     </ComponentPropsContext>
@@ -101,13 +105,6 @@ export const getStaticPaths: GetStaticPaths = async (context) => {
 // revalidation (or fallback) is enabled and a new request comes in.
 export const getStaticProps: GetStaticProps = async (context) => {
   const props = await sitecorePagePropsFactory.create(context);
-
-  // Check if we have a redirect (e.g. custom error page)
-  if (props.redirect) {
-    return {
-      redirect: props.redirect,
-    };
-  }
 
   return {
     props,
