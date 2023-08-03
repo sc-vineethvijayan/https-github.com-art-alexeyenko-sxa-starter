fdb6e30ea663589aa9f178911b0d1c41d52c49c6
Artem Alexeyenko <ala@sitecore.net>
2023-08-03 18:28:41 -0400

    Simple commit to trigger vercel deploy

--- src/sxastarter/src/byoc/DefaultByocComponent.tsx
+++ src/sxastarter/src/byoc/DefaultByocComponent.tsx
@@ -14,7 +14,6 @@ export const DefaultByocComponent = (props: DefaultByocComponentProps): JSX.Elem
   return (
     <div className="container">
       <h2>{props.title || 'BYOC Demo'}</h2>
-      <p>DefaultByocComponent Component</p>
       <div className="row">
         {columns.map((text, index) => (
           <div key={index} className={`col-sm-${props.columnsCount}`}>
