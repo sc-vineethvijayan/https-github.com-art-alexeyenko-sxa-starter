ac86f92ea836b5c8b98ceec0a661ed5837f5a755
Artem Alexeyenko <ala@sitecore.net>
2023-08-22 16:04:57 -0400

    Revert "revert getpublicurl"
    This reverts commit 138b0bd1daead6bdefd8a6f2c3f4b81aaa489fc6.

--- src/sxastarter/next.config.js
+++ src/sxastarter/next.config.js
@@ -1,6 +1,30 @@
 const jssConfig = require('./src/temp/config');
 const plugins = require('./src/temp/next-config-plugins') || {};
-const { getPublicUrl } = require('@sitecore-jss/sitecore-jss-nextjs/utils');
+
+const getPublicUrl = () => {
+  if (process.env.NETLIFY && process.env.DEPLOY_URL) {
+    return process.env.DEPLOY_URL;
+  }
+
+  if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`;
+
+  let url = process.env.PUBLIC_URL;
+  if (url === undefined) {
+    console.warn(
+      `'Warning:'
+       An PUBLIC_URL environment variable is not defined. Falling back to http://localhost:3000.`
+    );
+    url = 'http://localhost:3000';
+  } else {
+    try {
+      new URL(url);
+    } catch (error) {
+      throw new Error(`The PUBLIC_URL environment variable '${url}' is not a valid URL.`);
+    }
+  }
+  // Ensure no trailing slash
+  return url.toString().replace(/\/$/, '');
+};
 
 const publicUrl = getPublicUrl();
 
