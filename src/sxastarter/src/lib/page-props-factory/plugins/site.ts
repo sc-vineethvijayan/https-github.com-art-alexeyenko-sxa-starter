af5530e60f8cf1f9e92bdead99a4f199887b4012
Artem Alexeyenko <ala@sitecore.net>
2023-08-24 14:57:28 -0400

    alt regex test

--- src/sxastarter/src/lib/page-props-factory/plugins/site.ts
+++ src/sxastarter/src/lib/page-props-factory/plugins/site.ts
@@ -1,9 +1,13 @@
 import { SitecorePageProps } from 'lib/page-props';
 import { GetServerSidePropsContext, GetStaticPropsContext } from 'next';
-import { getSiteRewriteData } from '@sitecore-jss/sitecore-jss-nextjs';
 import { Plugin } from '..';
 import { siteResolver } from 'lib/site-resolver';
 import config from 'temp/config';
+export const SITE_PREFIX = '_site_';
+
+type SiteRewriteData = {
+  siteName: string;
+};
 
 class SitePlugin implements Plugin {
   order = 0;
@@ -29,6 +33,21 @@ class SitePlugin implements Plugin {
 
     return props;
   }
+}
+
+function getSiteRewriteData(pathname: string, defaultSiteName: string): SiteRewriteData {
+  const data = {
+    siteName: defaultSiteName,
+  };
+
+  const path = pathname.endsWith('/') ? pathname : pathname + '/';
+  const result = path.match(`${SITE_PREFIX}(.*?)\..+?\\/`);
+
+  if (result && result[1] !== '') {
+    data.siteName = result[1];
+  }
+
+  return data;
 }
 
 export const sitePlugin = new SitePlugin();
