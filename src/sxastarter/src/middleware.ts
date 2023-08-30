510486f8732acd490a9634d86d41ddba27c6e622
Artem Alexeyenko <ala@sitecore.net>
2023-08-30 14:56:05 -0400

    disable middleware

--- src/sxastarter/src/middleware.ts
+++ src/sxastarter/src/middleware.ts
@@ -1,9 +1,8 @@
-import type { NextRequest, NextFetchEvent } from 'next/server';
-import middleware from 'lib/middleware';
+import { NextResponse } from 'next/server';
 
 // eslint-disable-next-line
-export default async function (req: NextRequest, ev: NextFetchEvent) {
-  return middleware(req, ev);
+export default async function () {
+  return NextResponse.next();
 }
 
 export const config = {
