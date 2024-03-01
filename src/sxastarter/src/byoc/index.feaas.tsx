d3a4c26f51f463e377365ba7bd971754c82e8224
art-alexeyenko <art.alexeyenko@gmail.com>
2024-03-01 16:47:12 -0500

    update img override

--- src/sxastarter/src/byoc/index.feaas.tsx
+++ src/sxastarter/src/byoc/index.feaas.tsx
@@ -1,14 +1,14 @@
 import Image from 'next/image';
 import * as FEAAS from '@sitecore-feaas/clientside/react';
 
-// Register single react and nextjs components to be used in Component Builder
+// Element implementations for Sitecore Component Builder can be overriden here 
 
-// Register next Image to be used in Component Builder
+// Register next Image to be used in Component Builder. 
+// Nextjs image implementation will be used when img is rendered in component from Component Builder
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-FEAAS.setElementImplementation('img', ({ children, ...attributes }: any) => {
-  console.log(children);
-  return <Image {...attributes} />;
-});
+FEAAS.setElementImplementation('img', (attributes: any) => (
+    <Image {...attributes} />
+));
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {};
