9d5512d2c1125e44157b8b9a960cdf32719238dc
art-alexeyenko <art.alexeyenko@gmail.com>
2024-03-04 16:21:22 -0500

    test3

--- src/sxastarter/next.config.js
+++ src/sxastarter/next.config.js
@@ -56,7 +56,7 @@ const nextConfig = {
   // see https://nextjs.org/docs/app/api-reference/components/image#remotepatterns
   images: {
     remotePatterns: 
-      process.env.NODE_ENV === 'development' ? 
+      process.env.NODE_ENV === 'development' || process.env.SITECORE ? 
        remotePatternsDev : 
        remotePatternsProd
   },
